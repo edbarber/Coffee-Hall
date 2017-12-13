984cc9f4022b08a40c3b3eea8cddda70bf2cc0a7
Edward Barber <ebarber5969@conestogac.on.ca>
2017-12-12 23:55:20 -0500

    Fixed table output on get coffee page

--- www/wp-content/plugins/CoffeeOrderer/backend.php
+++ www/wp-content/plugins/CoffeeOrderer/backend.php
@@ -16,14 +16,41 @@ add_filter('rewrite_rules_array', function ($rules) {
 });
 
 function GetPurchasableCoffeeTable() {
-    $slim = new \Slim\Slim();
-    //do_action('slim_mapping',$slim);
     global $wpdb, $table_prefix;
-    echo($table_prefix . TABLE_IDENTIFIER . GET_COFFEE_TABLE_NAME);
-    echo json_encode(
-        $wpdb->get_results( 
-            $wpdb->prepare("SELECT * FROM " . 
-            $table_prefix . TABLE_IDENTIFIER . GET_COFFEE_TABLE_NAME, null)));
-    //$slim->run();
-    exit;
+    
+    // for some reason this is the only loop that doesn't crash wordpress :/
+
+    $counter = 1;
+    $tableName = $table_prefix . TABLE_IDENTIFIER . GET_COFFEE_TABLE_NAME;
+    $targetPageId = 22;
+    $tableRows = $wpdb->get_var(
+        $wpdb->prepare("SELECT COUNT(*) FROM " . $tableName, null));
+
+    while (true) {
+        if ($counter > $tableRows) {
+            break;
+        }
+        
+        echo '<section class="get-coffee-sections">';
+
+        echo 
+        '<h2>' . $wpdb->get_var(
+            $wpdb->prepare("SELECT title FROM " . $tableName . " WHERE id = " . $counter, null)
+        ) . '</h2>' .
+        '<a href="/index.php?page_id=' . $targetPageId . '">' . 
+        '<img src="' . 
+        $wpdb->get_var(
+            $wpdb->prepare("SELECT imageURL FROM " . $tableName . " WHERE id = " . $counter, null)
+        ) .
+        '" width="200px" height="200px alt="Server error">' .
+        '</a>'.
+        '<p>' . $wpdb->get_var(
+            $wpdb->prepare("SELECT description FROM " . $tableName . " WHERE id = " . $counter, null)
+        ) . '</p>' .
+        '<a class="custom-button" href="/index.php?page_id=' . $targetPageId . '">Customize</a>';
+        
+        echo '</section>';
+
+        $counter++;
+    }
 }
