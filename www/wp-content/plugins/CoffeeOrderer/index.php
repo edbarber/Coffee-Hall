fe1557fd49a81c3336c56f81c49d3fdfe6c4593f
Edward Barber <ebarber5969@conestogac.on.ca>
2018-01-07 13:27:00 -0500

    Fixed some issues with custom plugin and polish

--- www/wp-content/plugins/CoffeeOrderer/index.php
+++ www/wp-content/plugins/CoffeeOrderer/index.php
@@ -18,27 +18,21 @@ function orderer_shortcodes_init()
 add_action('init', 'orderer_shortcodes_init');
 
 add_action('init', function() {
-    add_shortcode('customize-coffee-form',function($atts = [], $content = null){
-        $content .= "<script>var atts = " .
-        json_encode($atts) .
-        "</script>";
-        $content .= file_get_contents(dirname(__FILE__) . "/order.php");
+    add_shortcode('customize-coffee-form', function($atts = [], $content = null) {
+        $content .= "<script>var atts = " . json_encode($atts) . "</script>";
+        $content .= file_get_contents(dirname(__FILE__) . "/order.html");
+        
         return $content;
     });
+    
     wp_register_style('style_style', plugins_url('style.css', __FILE__), true);
     wp_enqueue_style('style_style');
-    
+
     wp_register_script('angular_script', 
-    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.js", 
-    true);
-    
+        "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.js", true);
     wp_enqueue_script('angular_script');
-    wp_register_script('coffeecontroller_script', 
-    plugins_url('indexcontroller.js', __FILE__), 
-    true);
-
+    
+    wp_register_script('coffeecontroller_script', plugins_url('indexcontroller.js', __FILE__), 
+        true);
     wp_enqueue_script('coffeecontroller_script');
-   
 });
-
-
--- www/wp-content/plugins/CoffeeOrderer/indexcontroller.js
+++ www/wp-content/plugins/CoffeeOrderer/indexcontroller.js
@@ -1,18 +1,19 @@
-angular.module("coffeeApp", []).controller("coffeeCtrl", function ($scope, $http, $timeout) {
+angular.module("coffeeApp", []).controller("coffeeCtrl", function ($scope, $http) {
   $scope.model = {};
 
   $scope.submitForm = function () {    
     $http.post("/slim/api/orders", $scope.model).then(
       function (data) {
         $scope.model = {};      
-        $scope.message="Your order is placed!";
+        $scope.message = "Your order is placed!";
         $scope.showMessage = true;
-        $timeout(function(){
-           $scope.showMessage = false;
-        }, 3000);
-        
       }, function (err) {
-        $scope.message=JSON.stringify(err);
-      });
+        $scope.message = JSON.stringify(err);
+      }
+    );
+  }
+
+  $scope.goBack = function () {
+    window.history.back();
   }
 });
